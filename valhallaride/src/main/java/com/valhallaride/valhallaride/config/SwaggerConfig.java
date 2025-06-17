274e3733b7df0a8ebdc05af8d49609c104e515c3
JamesAVT <j.videlat@gmail.com>
2025-06-17 17:00:12 -0400

    Update SwaggerConfig.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/config/SwaggerConfig.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/config/SwaggerConfig.java
@@ -1,1 +1,21 @@
+package com.valhallaride.valhallaride.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API ValhallaRide")
+                        .version("1.0")
+                        .description("Documentación de la API para ValhallaRide"));
+    }
+}
 
