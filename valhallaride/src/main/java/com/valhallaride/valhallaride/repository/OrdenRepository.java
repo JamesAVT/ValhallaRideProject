8d5b01ba3b76e038f6afb959be0ba93b55049dd7
JamesAVT <j.videlat@gmail.com>
2025-06-20 15:41:25 -0400

    Update OrdenRepository.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/repository/OrdenRepository.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/repository/OrdenRepository.java
@@ -1,6 +1,9 @@
 package com.valhallaride.valhallaride.repository;
 
+import com.valhallaride.valhallaride.model.MetodoPago;
 import com.valhallaride.valhallaride.model.Orden;
+import com.valhallaride.valhallaride.model.Usuario;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +15,9 @@ public interface OrdenRepository extends JpaRepository<Orden, Integer> {
     List<Orden> findByUsuario_IdUsuario(Integer idUsuario);
 
     List<Orden> findByPagado(Boolean pagado);
-    
-    List<Orden> findByMetodoPago(MetodoPago metodopago);
-    
+
+    List<Orden> findByMetodoPago(MetodoPago metodoPago);
+
+    List<Orden> findByUsuario(Usuario usuario);
+
 }
