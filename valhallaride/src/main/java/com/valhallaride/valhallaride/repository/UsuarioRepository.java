309b8403ce38128e6046503211a65b621ea60ad2
MauriLS <mauriciolillo122@gmail.com>
2025-06-22 18:41:47 -0400

    Update UsuarioRepository.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/repository/UsuarioRepository.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/repository/UsuarioRepository.java
@@ -11,4 +11,13 @@ public interface  UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<Usuario> findByNombreUsuario(String nombreUsuario); // Chicos! Este devuelve una lista porque puede haber varios usuarios con el mismo nombre
     Optional<Usuario> findByCorreoUsuario(String correoUsuario); // En este caso, usamos "Optional", ya que el correo es unico, y tambien esta la posibilidad de que no exista
     List<Usuario> findByRol_idRol(Integer idRol); // Aqui buscamos todos los usuarios que tengan un rol específico (por el id de rol)
+
+    // Query 4 - 3 tablas Usuarios con su rol y total de órdenes realizadas
+    @Query("SELECT u.nombreUsuario, r.nombreRol, COUNT(o) " +
+       "FROM Usuario u " +
+       "JOIN u.rol r " +
+       "LEFT JOIN u.ordenes o " +
+       "GROUP BY u.nombreUsuario, r.nombreRol")
+    List<Object[]> listarUsuariosConRolYTotalOrdenes();
+
 }
