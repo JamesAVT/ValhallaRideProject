ed0485bf2b33a6ff6365ce94d0e8c5249067f6be
JamesAVT <j.videlat@gmail.com>
2025-06-22 18:04:35 -0400

    Update OrdenControllerv2.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/controller/OrdenControllerv2.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/controller/OrdenControllerv2.java
@@ -36,41 +36,41 @@ public class OrdenControllerv2 {
     private OrdenModelAssembler assembler;
 
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
-    public CollectionModel<EntityModel<Orden>> getAllOrdenes() {
-        List<EntityModel<Orden>> ordenes = ordenService.findAll().stream()
-            .map(assembler::toModel)
-            .collect(Collectors.toList());
+    public CollectionModel<EntityModel<Orden>> getAllOrdenes() { // Devuelve un "ColectionModel" que contiene una lista de órdenes con enlaces HATEOAS
+        List<EntityModel<Orden>> ordenes = ordenService.findAll().stream() // Llama a service para obtener todas las órdenes, y usa stream() para recorrer cada una
+            .map(assembler::toModel) // Convierte cada orden en un EntityModel
+            .collect(Collectors.toList()); // Se recopila todos esos modelos en una lista
 
-        return CollectionModel.of(ordenes,
-            linkTo(methodOn(OrdenControllerv2.class).getAllOrdenes()).withSelfRel());
+        return CollectionModel.of(ordenes, // Devuelve un CollectionModel que contiene la lista de órdenes
+            linkTo(methodOn(OrdenControllerv2.class).getAllOrdenes()).withSelfRel()); // Incluye un enlace "self" que apunta a getAllOrdenes, permitiendo al cliente saber cómo volver a obtener esta colección
     }
 
     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
-    public EntityModel<Orden> getOrdenById(@PathVariable Integer id) {
-        Orden orden = ordenService.findById(id);
-        return assembler.toModel(orden);
+    public EntityModel<Orden> getOrdenById(@PathVariable Integer id) { // Obtiene el ID de la orden, y devuelve EntityModel que es la orden con enlaces HATEOAS
+        Orden orden = ordenService.findById(id); // Llama a service para buscar la orden por ID
+        return assembler.toModel(orden); // Incluye enlaces como put, delete, etc.
         }
 
     @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
     public ResponseEntity<EntityModel<Orden>> crearOrden(@RequestBody Orden orden) {
-        Orden nuevaOrden = ordenService.save(orden);
+        Orden nuevaOrden = ordenService.save(orden); // Guarda una orden usando service
         return ResponseEntity
-            .created(linkTo(methodOn(OrdenControllerv2.class).getOrdenById(nuevaOrden.getIdOrden())).toUri())
-            .body(assembler.toModel(nuevaOrden));
+            .created(linkTo(methodOn(OrdenControllerv2.class).getOrdenById(nuevaOrden.getIdOrden())).toUri()) // Usa HATEOAS para construir el enlace hacia la nueva orden creada y genera la URI apuntando a getOrdenById
+            .body(assembler.toModel(nuevaOrden)); // Contiene todos los enlaces de HATEOAS (update, delete, etc)
         }
     
     @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     public ResponseEntity<EntityModel<Orden>> actualizarOrden(@PathVariable Integer id, @RequestBody Orden orden) {
-        orden.setIdOrden(id);
-        Orden ordenActualizada = ordenService.save(orden);
+        orden.setIdOrden(id); // Se asegura de que el objeto orden tenga el ID correcto antes de guardarlo
+        Orden ordenActualizada = ordenService.save(orden); // Guarda la orden
         return ResponseEntity
-                .ok(assembler.toModel(ordenActualizada));
+                .ok(assembler.toModel(ordenActualizada)); // Devuelve 200 OK e incluye enlaces de HATEOAS(update, delete, etc)
 
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
-        public ResponseEntity<?> eliminarOrden(@PathVariable Integer id) {
-            ordenService.delete(id);
-            return ResponseEntity.noContent().build();
+        public ResponseEntity<?> eliminarOrden(@PathVariable Integer id) { // Método que recibe una ID para eliminar una orden
+            ordenService.delete(id); // Llama a service para eliminar la orden con el ID dado
+            return ResponseEntity.noContent().build(); // Devuelve un 204 No Content 
         }
 }
