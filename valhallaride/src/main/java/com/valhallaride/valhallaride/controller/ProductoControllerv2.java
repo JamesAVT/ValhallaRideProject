3cf161f42fb08e3b6e66524524566b7d8cdf9cb6
JamesAVT <j.videlat@gmail.com>
2025-06-22 18:05:22 -0400

    Update ProductoControllerv2.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/controller/ProductoControllerv2.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/controller/ProductoControllerv2.java
@@ -35,40 +35,41 @@ public class ProductoControllerv2 {
     private ProductoModelAssembler assembler;
 
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
-    public CollectionModel<EntityModel<Producto>> getAllProductos(){
-        List<EntityModel<Producto>> productos = productoService.findAll().stream()
-            .map(assembler::toModel)
+    public CollectionModel<EntityModel<Producto>> getAllProductos(){ // Devuelve una colección de productos, donde cada producto tiene enlaces HATEOAS
+        List<EntityModel<Producto>> productos = productoService.findAll().stream() // Llama a service para obtener todos los productos, y se usa stream() para convertir cada producto en un EntityModel
+            .map(assembler::toModel) // Agrega los enlaces HATEOAS (actualizar, eliminar, etc)
             .collect(Collectors.toList());
         return CollectionModel.of(productos,
-                linkTo(methodOn(ProductoControllerv2.class).getAllProductos()).withSelfRel());
+                linkTo(methodOn(ProductoControllerv2.class).getAllProductos()).withSelfRel()); // Crea una colección HATEOAS con la lista de productos y un enlace "withSelfRel()", que permite que el cliente sepa de dónde vino esa colección
     }
 
     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     public EntityModel<Producto> getProductoById(@PathVariable Integer id){
-        Producto producto = productoService.findById(id);
-        return assembler.toModel(producto);
+        Producto producto = productoService.findById(id); // Llama a Service para buscar el producto usando el ID
+        return assembler.toModel(producto); // Devuelve el producto en un EntityModel (assembler), agregando enlaces HATEOAS (actualizar, eliminar, etc)
     }
 
     @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
     public ResponseEntity<EntityModel<Producto>> crearProducto(@RequestBody Producto producto){
-        Producto nuevoProducto = productoService.save(producto);
-        return ResponseEntity
+        Producto nuevoProducto = productoService.save(producto); // Guarda el nuevo producto 
+        return ResponseEntity 
                 .created(linkTo(methodOn(ProductoControllerv2.class).getProductoById(nuevoProducto.getIdProducto())).toUri())
-                .body(assembler.toModel(nuevoProducto));
-    }
+                .body(assembler.toModel(nuevoProducto)); // Usa HATEOAS para construir un enlace al producto recién creado (getProductoById)
+    }                                                   // toUri() convierte ese enlace en una URL. created genera una respuesta 201 created y devuelve el producto
+
 
     @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
     public ResponseEntity<EntityModel<Producto>> actualizarProducto(@PathVariable Integer id, @RequestBody Producto producto){
-        producto.setIdProducto(id);
-        Producto productoActualizado = productoService.save(producto);
+        producto.setIdProducto(id); // Asegura que el objeto producto tenga el ID correcto que viene por la URL (para evitar que se cree uno nuevo por error)
+        Producto productoActualizado = productoService.save(producto); // Guarda el prdocuto 
         return ResponseEntity
-                .ok(assembler.toModel(productoActualizado));   
+                .ok(assembler.toModel(productoActualizado)); // Devuelve 200 OK con el producto actualizado, convertido en EntityModel para incluir enlaces HATEOAS
 
         }
 
     @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
         public ResponseEntity<?> eliminarProducto(@PathVariable Integer id){
-            productoService.delete(id);
-            return ResponseEntity.noContent().build();
+            productoService.delete(id); // Llama a service para eliminar el producto identificado por el ID
+            return ResponseEntity.noContent().build(); // Devuelve respuesta 204 No Content, indicando que se eliminó correctamente
         }
 }
