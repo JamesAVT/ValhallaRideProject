5eb69c82a74d43647f5e1fe24a7a68284cd81cc9
JamesAVT <j.videlat@gmail.com>
2025-06-22 20:26:34 -0400

    Update TiendaModelAssembler.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/assemblers/TiendaModelAssembler.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/assemblers/TiendaModelAssembler.java
@@ -13,13 +13,18 @@ import com.valhallaride.valhallaride.model.Tienda;
 @Component
 public class TiendaModelAssembler implements RepresentationModelAssembler<Tienda, EntityModel<Tienda>>{
 
-    @Override
-    public EntityModel<Tienda> toModel(Tienda tienda){
-        return EntityModel.of(tienda,
+    @Override // Se sobrescribe el método toModel()
+    public EntityModel<Tienda> toModel(Tienda tienda){ // Convierte un objeto "Tienda" en un EntityModel (enlaces HATEOAS)
+        return EntityModel.of(tienda, // Se crea el EntityModel
+        // Enlace a la tienda específica por ID
         linkTo(methodOn(TiendaControllerv2.class).getTiendaById(Long.valueOf(tienda.getIdTienda()))).withSelfRel(),
+        // Enlace que permite obtener todas las tiendas
         linkTo(methodOn(TiendaControllerv2.class).getAllTiendas()).withRel("tiendas"),
+        // Enlace que permite crear una tienda
         linkTo(methodOn(TiendaControllerv2.class).crearTienda(tienda)).withRel("crear_tienda"),
+        // Enlace que permite actualizar una tienda
         linkTo(methodOn(TiendaControllerv2.class).actualizarTienda(Long.valueOf(tienda.getIdTienda()), tienda)).withRel("actualizar_tienda"),
+        // Enlace que permite eliminar una tienda
         linkTo(methodOn(TiendaControllerv2.class).eliminarTienda(Long.valueOf(tienda.getIdTienda()))).withRel("eliminar_tienda"));
     }
 }
