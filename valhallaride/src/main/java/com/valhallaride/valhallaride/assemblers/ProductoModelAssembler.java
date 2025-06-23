c737896fd7cab23dd549fd1926278db63cdfce3b
JamesAVT <j.videlat@gmail.com>
2025-06-22 20:16:03 -0400

    Update ProductoModelAssembler.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/assemblers/ProductoModelAssembler.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/assemblers/ProductoModelAssembler.java
@@ -10,16 +10,21 @@ import org.springframework.stereotype.Component;
 import com.valhallaride.valhallaride.controller.ProductoControllerv2;
 import com.valhallaride.valhallaride.model.Producto;
 
-@Component
+@Component // Marca esta clase como un componente de Spring
 public class ProductoModelAssembler implements RepresentationModelAssembler <Producto, EntityModel<Producto>>{
 
-    @Override
-    public EntityModel<Producto> toModel(Producto producto) {
-            return EntityModel.of(producto,
+    @Override // Método que sobrescrobe el toModel()
+    public EntityModel<Producto> toModel(Producto producto) { // Recibe un objeto producto y devuelve un EntityModel, con enlaces HATEOAS
+            return EntityModel.of(producto, // Se crea un EntityModel que envuelve al producto
+            // Enlace al endpoit que devuelve este mismo producto por su ID
             linkTo(methodOn(ProductoControllerv2.class).getProductoById(producto.getIdProducto())).withSelfRel(),
+            // Enlace que obtiene todos los productos
             linkTo(methodOn(ProductoControllerv2.class).getAllProductos()).withRel("productos"),
+            // Enlace que permite crear un producto
             linkTo(methodOn(ProductoControllerv2.class).crearProducto(producto)).withRel("crear_producto"),
+            // Enlace que permite actualizar un producto
             linkTo(methodOn(ProductoControllerv2.class).actualizarProducto(producto.getIdProducto(), producto)).withRel("actualizar_producto"),
+            // Enlace que permite eliminar un producto
             linkTo(methodOn(ProductoControllerv2.class).eliminarProducto(producto.getIdProducto())).withRel("eliminar_producto"));
     }
 }
