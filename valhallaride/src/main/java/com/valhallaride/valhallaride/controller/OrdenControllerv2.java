3133449acac99d7bb724cb3b3b7b46a01dbbf631
JamesAVT <j.videlat@gmail.com>
2025-06-22 23:22:25 -0400

    Update OrdenControllerv2.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/controller/OrdenControllerv2.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/controller/OrdenControllerv2.java
@@ -20,10 +20,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Map;
 import com.valhallaride.valhallaride.assemblers.OrdenModelAssembler;
 import com.valhallaride.valhallaride.model.Orden;
 import com.valhallaride.valhallaride.service.OrdenService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/v2/ordenes")
@@ -36,6 +38,7 @@ public class OrdenControllerv2 {
     private OrdenModelAssembler assembler;
 
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Obtener todas las ordenes", description = "Obtiene una lista de todas las ordenes ")
     public CollectionModel<EntityModel<Orden>> getAllOrdenes() { // Devuelve un "ColectionModel" que contiene una lista de órdenes con enlaces HATEOAS
         List<EntityModel<Orden>> ordenes = ordenService.findAll().stream() // Llama a service para obtener todas las órdenes, y usa stream() para recorrer cada una
             .map(assembler::toModel) // Convierte cada orden en un EntityModel
@@ -46,17 +49,20 @@ public class OrdenControllerv2 {
     }
 
     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Obtener Ordenes por su ID", description = "Obtiene una orden existente")
     public EntityModel<Orden> getOrdenById(@PathVariable Integer id) { // Obtiene el ID de la orden, y devuelve EntityModel que es la orden con enlaces HATEOAS
         Orden orden = ordenService.findById(id); // Llama a service para buscar la orden por ID
         return assembler.toModel(orden); // Incluye enlaces como put, delete, etc.
         }
 
     @GetMapping("/ordenes-detalladas")
+    @Operation(summary = "Ordenes con método de pago, tienda y total productos", description = "Obtiene órdenes detalladas incluyendo método de pago, tienda y cantidad total de productos")
     public ResponseEntity<List<Map<String, Object>>> obtenerOrdenesDetalladas() {
         return ResponseEntity.ok(ordenService.listarOrdenesDetalladas());
     }
     
     @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Crear Orden", description = "Crea una orden")
     public ResponseEntity<EntityModel<Orden>> crearOrden(@RequestBody Orden orden) {
         Orden nuevaOrden = ordenService.save(orden); // Guarda una orden usando service
         return ResponseEntity
@@ -65,6 +71,7 @@ public class OrdenControllerv2 {
         }
     
     @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Actualizar Orden por su ID", description = "Actualiza todos los datos de una orden existente")
     public ResponseEntity<EntityModel<Orden>> actualizarOrden(@PathVariable Integer id, @RequestBody Orden orden) {
         orden.setIdOrden(id); // Se asegura de que el objeto orden tenga el ID correcto antes de guardarlo
         Orden ordenActualizada = ordenService.save(orden); // Guarda la orden
@@ -74,6 +81,7 @@ public class OrdenControllerv2 {
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Eliminar Orden por su ID", description = "Elimina una orden existente")
         public ResponseEntity<?> eliminarOrden(@PathVariable Integer id) { // Método que recibe una ID para eliminar una orden
             ordenService.delete(id); // Llama a service para eliminar la orden con el ID dado
             return ResponseEntity.noContent().build(); // Devuelve un 204 No Content 
