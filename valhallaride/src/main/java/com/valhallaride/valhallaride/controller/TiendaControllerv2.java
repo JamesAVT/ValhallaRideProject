3b3c141e4d6268d8022c5c223578cb075d965558
Cri008d <cri.zunigam@duocuc.cl>
2025-06-22 23:20:19 -0400

    Update TiendaControllerv2.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/controller/TiendaControllerv2.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/controller/TiendaControllerv2.java
@@ -26,8 +26,12 @@ import com.valhallaride.valhallaride.service.TiendaService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v2/tiendas")
+@Tag(name = "Tiendas", description = "Operaciones relacionadas con Tiendas")
 public class TiendaControllerv2 {
 
     @Autowired
@@ -37,6 +41,7 @@ public class TiendaControllerv2 {
     private TiendaModelAssembler assembler;
 
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Obtener todas las tiendas", description = "Obtiene una lista de todas las tiendas")
     public CollectionModel<EntityModel<Tienda>> getAllTiendas(){ // Método para obtener todas las tiendas
         List<EntityModel<Tienda>> tiendas = tiendaService.findAll().stream() // Obtiene la lista de todas las tiendas llamando a service. stream transforma cada objeto Tienda en un EntityModel con enlaces
         .map(assembler::toModel)
@@ -46,12 +51,14 @@ public class TiendaControllerv2 {
     }
 
     @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Obtener una tienda por ID", description = "Obtiene una tienda especificada por su ID")
     public EntityModel<Tienda> getTiendaById(@PathVariable Long id){ // Método para obtener una tienda específica por su ID
             Tienda tienda = tiendaService.findById(id); // Llama a service para buscar la tienda por su ID
             return assembler.toModel(tienda); // Devuelve el objeto Tienda en un EntityModel, que tiene enlaces HATEOAS (actualizar, eliminar, etc)
     }
 
     @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Crear Tienda", description = "Crea una nueva tienda")
     public ResponseEntity<EntityModel<Tienda>> crearTienda(@RequestBody Tienda tienda){ // Método para crear una nueva tienda
         Tienda nuevaTienda = tiendaService.save(tienda); // Llama a service para guardar la nueva tienda
         return ResponseEntity // Devuelve 200 OK
@@ -60,6 +67,7 @@ public class TiendaControllerv2 {
     }
 
     @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Actualizar Tienda por su ID", description = "Actualiza todos los datos de una tienda existente")
     public ResponseEntity<EntityModel<Tienda>> actualizarTienda(@PathVariable Long id, @RequestBody Tienda tienda){ // Método para actualizar una tienda por su ID
         tienda.setIdTienda(id); // Asigna el ID recibido al objeto tienda para asegurar que se actualice la tienda correcta
         Tienda tiendaActualizada = tiendaService.save(tienda); // Llama a service para guardar (actualizar) la tienda con los datos dados
@@ -69,6 +77,7 @@ public class TiendaControllerv2 {
         }
 
     @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    @Operation(summary = "Eliminar Tienda por ID", description = "Elimina una tienda existente")
         public ResponseEntity<?> eliminarTienda(@PathVariable Long id){ // Método para eliminar una tienda por su ID
             tiendaService.delete(id); // Llama a service para eliminar la tienda con el ID especificado
             return ResponseEntity.noContent().build(); // Devuelve respuesta 204 No Content
