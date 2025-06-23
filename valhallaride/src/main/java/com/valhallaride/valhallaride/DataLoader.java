0a8e4e649fc9aa8d67275542ffe00664512fb264
JamesAVT <j.videlat@gmail.com>
2025-06-22 22:54:35 -0400

    Update DataLoader.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/DataLoader.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/DataLoader.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
+
 import java.util.List;
 import java.util.Random;
 
