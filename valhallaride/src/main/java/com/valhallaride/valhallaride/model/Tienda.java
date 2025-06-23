df4821d66a5aabdd165003bbac04abc5e2206769
JamesAVT <j.videlat@gmail.com>
2025-06-22 22:53:55 -0400

    Update Tienda.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/model/Tienda.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/model/Tienda.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
