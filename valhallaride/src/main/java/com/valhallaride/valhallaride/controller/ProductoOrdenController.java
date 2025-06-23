550eec62cab93a88995840f95c904607cd6e4494
MauriLS <mauriciolillo122@gmail.com>
2025-06-22 21:25:03 -0400

    Update ProductoOrdenController.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/controller/ProductoOrdenController.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/controller/ProductoOrdenController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.valhallaride.valhallaride.service.ProductoOrdenService;
-
+import java.util.Map;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
