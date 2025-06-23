305a7c94ec7836b441395eada77a012c8f1e159d
MauriLS <mauriciolillo122@gmail.com>
2025-06-22 21:24:45 -0400

    Update ProductoController.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/controller/ProductoController.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/controller/ProductoController.java
@@ -1,7 +1,7 @@
 package com.valhallaride.valhallaride.controller;
 
 import java.util.List;
-
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
