ae73715cdfe9a08f78d8257bfa5543991c10b5c2
JamesAVT <j.videlat@gmail.com>
2025-06-22 20:01:59 -0400

    Update CategoriaModelAssembler.java

--- valhallaride/src/main/java/com/valhallaride/valhallaride/assemblers/CategoriaModelAssembler.java
+++ valhallaride/src/main/java/com/valhallaride/valhallaride/assemblers/CategoriaModelAssembler.java
@@ -11,16 +11,21 @@ import com.valhallaride.valhallaride.controller.CategoriaControllerv2;
 import com.valhallaride.valhallaride.model.Categoria;
 
 
-@Component
+@Component // Marca esta clase como un componente de Spring
 public class CategoriaModelAssembler implements RepresentationModelAssembler<Categoria, EntityModel<Categoria>> {
 
-    @Override
-    public EntityModel<Categoria> toModel(Categoria categoria) {
-        return EntityModel.of(categoria,
+    @Override // Este método sobrescribe toModel()
+    public EntityModel<Categoria> toModel(Categoria categoria) { // Convierte categoría en un EntityModel, que es un objeto con enlaces (HATEOAS)
+        return EntityModel.of(categoria, // Se crea el EntityModel
+                // Agrega un enlace self, apuntando al método que obtiene esta misma categoría por su ID
                 linkTo(methodOn(CategoriaControllerv2.class).getCategoriaById(Long.valueOf(categoria.getIdCategoria()))).withSelfRel(),
+                // Agrega un link con la relación de categorías, que apunta para listar todas las categorías
                 linkTo(methodOn(CategoriaControllerv2.class).getAllCategorias()).withRel("categorias"),
+                // Agrega un link para crear una categoría
                 linkTo(methodOn(CategoriaControllerv2.class).crearCategoria(categoria)).withRel("crar_categoria"),
+                // Enlace para actualizar la categoría
                 linkTo(methodOn(CategoriaControllerv2.class).actualizarCategoria(Long.valueOf(categoria.getIdCategoria()), categoria)).withRel("actualizar_categoria"),
+                // Enlace para eliminar categoría
                 linkTo(methodOn(CategoriaControllerv2.class).eliminarCategoria(Long.valueOf(categoria.getIdCategoria()))).withRel("eliminar_categoria"));
     }
 }
